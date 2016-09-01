072dd0a076ea39c5e8468107abbe1de06fa28a61
tzhu65 <tzhu65@gmail.com>
2016-08-31 20:44:38 -0400

    test

--- bin/deploy-node.sh
+++ bin/deploy-node.sh
@@ -5,18 +5,18 @@ NODE=$2
 CRAWLER=$3
 
 all_playbooks=false
-if [ "${NODE}" = "all"]; then
+if [ "${NODE}" = "all" ]; then
   all_playbooks=true
 fi
 
 if [ "${NODE}" = "nginx" ] || [ "${all_playbooks}" = true]; then
   /etc/ansible/bin/ansible-playbook -i /etc/ansible/hosts \
-  --extra-vars="target=${TARGET}" \
+  --extra-vars="target=${TARGET} docker_host_ip=${DOCKER_HOST_IP}" \
   /opt/deploy/ansible/playbooks/deploy/deploy-nginx.yml
 
 elif [ "${NODE}" = "scrapy" ] || [ "${all_playbooks}" = true]; then
   /etc/ansible/bin/ansible-playbook -i /etc/ansible/hosts \
-  --extra-vars="target=${TARGET} docker_host_ip=${DOCKER_HOST_IP} crawler=${CRAWLER}" \
+  --extra-vars="target=${TARGET} crawler=${CRAWLER}" \
   /opt/deploy/ansible/playbooks/deploy/deploy-scrapy.yml
 
 fi
